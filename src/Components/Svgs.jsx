aaafc8f15725f1f3a5079296484780309caeb5bf
Dacron <favouradebimpe63@gmail.com>
2024-06-22 07:42:05 +0100

    trying backgroundImages agian

--- src/Components/Svgs.jsx
+++ src/Components/Svgs.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import patternQuotes from "/src/images/pattern-quotes.svg";
-import patternBg from "/src/images/pattern-bg.svg";
-import patternCurve from "/src/images/pattern-curve.svg";
+import patternQuotes from "../images/pattern-quotes.svg";
+import patternBg from "../images/pattern-bg.svg";
+import patternCurve from "../images/pattern-curve.svg";
 import { Box } from "@mui/material";
 
 function Svgs() {
@@ -19,7 +19,7 @@ function Svgs() {
             left: { xs: "110px", sm: "40px", lg: "50px" },
             width: { xs: "70px", sm: "300px", lg: "150px" },
             height: { xs: "80px", sm: "100px", lg: "150px" },
-            backgroundImage: `url(/src/images/pattern-quotes.svg)`,
+            backgroundImage: `url(${patternQuotes})`,
             backgroundRepeat: "no-repeat",
             backgroundSize: "contain",
           },
@@ -38,7 +38,7 @@ function Svgs() {
             left: { xs: 0, sm: "284px", lg: "550px" },
             width: { xs: "330px", sm: "400px", lg: "600px" },
             height: { xs: "300px", sm: "500px" },
-            backgroundImage: `url(/src/images/pattern-bg.svg)`,
+            backgroundImage: `url(${patternBg})`,
             backgroundRepeat: "no-repeat",
             backgroundSize: "contain",
           },
@@ -57,7 +57,7 @@ function Svgs() {
             left: 0,
             width: "100vw",
             height: "100px",
-            backgroundImage: `url(/src/images/pattern-curve.svg)`,
+            backgroundImage: `url(${patternCurve})`,
             backgroundRepeat: "no-repeat",
             backgroundSize: "contain",
             zIndex: "-1",
